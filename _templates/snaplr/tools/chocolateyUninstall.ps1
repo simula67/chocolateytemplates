de65549c0d68d2cdb8ce075949ae2ae6e3e288ba
Joji Antony <simula67@gmail.com>
2014-12-09 12:04:44 +0530

    Uninstall

---
+++ _templates/snaplr/tools/chocolateyUninstall.ps1
@@ -0,0 +1,20 @@
+﻿$package="Snaplr"
+$app = Get-WmiObject -Class Win32_Product | Where-Object { 
+    $_.Name -match $package 
+}
+if( $app )
+{
+   $uninstallReturnValue = $app.Uninstall().ReturnValue
+   If($uninstallReturnValue -eq 0)
+   {
+      Write-ChocolateySuccess $package
+   }
+   else
+   {
+      Write-ChocolateyFailure $package "$($_.Exception.Message)"
+   }
+}
+else
+{
+   Write-ChocolateyFailure "$package is not installed"
+}
