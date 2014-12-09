a9b386a81c7baa7a2f618eaaf96638d867a13014
Joji Antony <simula67@gmail.com>
2014-12-09 10:55:37 +0530

    Enable more targets
    32 bit machines
    Machines without C: drive

--- _templates/snaplr/tools/chocolateyInstall.ps1
+++ _templates/snaplr/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'snaplr'
 $installerType = 'EXE'
 $url = 'http://www.vessenger.com/assets/downloads/Snaplr.exe'
-$silentArgs = '-silent  -eulaAccepted -location "C:\Program Files (x86)\Snaplr" -desktopShortcut -programMenu -allowDownload'
+$silentArgs = "-silent -eulaAccepted -location ""$env:ProgramFiles"" -desktopShortcut -programMenu -allowDownload"
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
